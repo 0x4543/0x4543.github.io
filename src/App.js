62796b658c78e6b07a9ade3eba4adc0cf928ce69
0x4543 <dormin.eth@ethermail.io>
2023-12-22 23:59:15 -0500

    add info to Home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@
 
 import React from "react";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
-import LatestNews from "./pages/LatestNews";
 import ContactForm from "./pages/ContactForm";
 import "./styles.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
+    <div className="home-container">
+      <div className="profile">
+        <img
+          src="https://i.seadn.io/gcs/files/ddf3cdacad17f9b55269a6a5d1232a7a.jpg"
+          alt="Dormin Profile Pic"
+          className="profile-pic"
+        />
+        <div className="profile-info">
+          <h1>Dormin</h1>
+          <p>Crypto 💎 Fanaticus</p>
+          <a
+            href="https://link3.to/serious"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Web3 links hub
+          </a>
+        </div>
+      </div>
+      <p className="welcome-text">
+        Welcome to my website! Here, you'll find information about me and a way
+        to get in touch. Explore the world of crypto with me and stay updated on
+        the latest trends and discussions.
+      </p>
+      <p className="welcome-text">
+        <Link to="/contact/">Feel free to reach out and connect!</Link>
+      </p>
     </div>
   );
 };
@@ -24,9 +48,6 @@ const App = () => {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/latest-news/">Latest News</Link>
-            </li>
-            <li>
               <Link to="/contact/">Contact Me</Link>
             </li>
           </ul>
@@ -34,7 +55,6 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/latest-news/" element={<LatestNews />} />
           <Route path="/contact/" element={<ContactForm />} />
         </Routes>
       </div>
